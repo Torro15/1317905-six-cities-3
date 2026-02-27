aab13065df1288a6749f9cc55db29be5c2a43d3f
Светлана Г <svetlgrish28@gmail.com>
2026-02-27 21:46:42 +0300

    Добавит компонент футера

--- src/pages/favorites/favorites-page.tsx
+++ src/pages/favorites/favorites-page.tsx
@@ -1,4 +1,5 @@
 import Header from '../../components/header';
+import Footer from '../../components/footer';
 
 
 function FavoritesPage(): JSX.Element {
@@ -135,11 +136,7 @@ function FavoritesPage(): JSX.Element {
           </section>
         </div>
       </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width={64} height={33} />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
