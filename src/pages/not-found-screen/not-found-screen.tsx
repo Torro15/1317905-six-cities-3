50aff4f189bf8d534d868a46b05ad933f697fbcf
Светлана Г <svetlgrish28@gmail.com>
2026-03-03 11:56:35 +0300

    Добавит компонент страницы 404

---
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,22 @@
+
+import Header from '../../components/header.tsx';
+
+
+type NotFoundScreenPageProps = {
+  isAuth: boolean;
+}
+
+function NotFoundScreenPage({isAuth}: NotFoundScreenPageProps): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <Header isAuth={isAuth}/>
+
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">404 Not Found</h1>
+        <a href="/">Вернуться на главную</a>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundScreenPage;
