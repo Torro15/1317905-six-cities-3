519fbdf45bb63ecd8b1be6e781f9effcd13f2f39
Светлана Г <svetlgrish28@gmail.com>
2026-04-03 14:56:23 +0300

    Удалит моки из index.tsx. Создаст компонент для отображения ошибки и подключит его

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Setting } from './const';
-import { offersMock } from './mocks/offers';
-import { reviewsMock } from './mocks/reviews';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { store } from './store';
-import { loadOffers } from './store/action';
-import { offerCardsMock } from './mocks/offer-cards';
+import ErrorMessage from './components/error-message/error-message';
 
-store.dispatch(loadOffers(offerCardsMock));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -19,11 +15,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App
-        offers={offersMock}
-        reviews={reviewsMock}
-        offerCards={offerCardsMock}
         nearbyOffersCount={Setting.NearOffers}
       />
+      <ErrorMessage />
     </Provider>
   </React.StrictMode>,
 );
