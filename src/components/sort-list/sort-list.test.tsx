d411b26deaef5a57741d1b07159c0026010bfebf
Светлана Г <svetlgrish28@gmail.com>
2026-04-18 12:51:10 +0300

    Синтаксические правки

--- src/components/sort-list/sort-list.test.tsx
+++ src/components/sort-list/sort-list.test.tsx
@@ -1,4 +1,3 @@
-// src/components/sort-list/sort-list.test.tsx
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
